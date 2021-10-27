318b06c9f2d5c1cbbad41b1db3a5c18464eec025
Howard Wang <idontwannarock@gmail.com>
2021-10-27 14:11:40 +0800

    feat: add bit wise operation AndHexTwo

---
+++ csharp-example-test/DataType/BitWiseOperationTest.cs
@@ -0,0 +1,15 @@
+﻿using csharp_example.DataType;
+using NUnit.Framework;
+
+namespace csharp_example_test.DataType
+{
+    public class BitWiseOperationTest
+    {
+        [Test]
+        public void Int_And_HexTwo_Should_Not_Equal_To_One()
+        {
+            var bitWiseOperation = new BitWiseOperation();
+            Assert.IsFalse(bitWiseOperation.AndHexTwo(1) == 1);
+        }
+    }
+}
---
+++ csharp-example/DataType/BitWiseOperation.cs
@@ -0,0 +1,10 @@
+namespace csharp_example.DataType
+{
+    public class BitWiseOperation
+    {
+        public int AndHexTwo(int integer)
+        {
+            return integer & 0x2;
+        }
+    }
+}
